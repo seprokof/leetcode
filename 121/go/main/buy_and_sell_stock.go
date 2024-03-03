d9f7f2db05365a462effaf8adb4a13f33aa13de2
Sergey Prokofiev <seprokof@gmail.com>
2024-03-03 23:58:41 +0300

    121 / Go

---
+++ 121/go/main/buy_and_sell_stock.go
@@ -0,0 +1,13 @@
+package main
+
+func MaxProfit(prices []int) int {
+	buy, profit := prices[0], 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < buy {
+			buy = prices[i]
+		} else if prices[i]-buy > profit {
+			profit = prices[i] - buy
+		}
+	}
+	return profit
+}
