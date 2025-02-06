4d384db1213e3ec7f556329206f50d1379b7385d
Sergey Prokofiev <seprokof@gmail.com>
2025-02-06 19:29:12 +0300

    145 / Java

---
+++ 145/java/Solution.java
@@ -0,0 +1,80 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+class Solution {
+
+    public static void main(String[] args) {
+        // @formatter:off
+        record TestCase(TreeNode in, List<Integer> expected) {}
+
+        TestCase[] tests = {
+                new TestCase(makeTree(new Integer[] { 1, null, 2, 3 }), List.of(3, 2, 1)),
+                new TestCase(makeTree(new Integer[] { 1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9 }), List.of(4, 6, 7, 5, 2, 9, 8, 3, 1)),
+                new TestCase(makeTree(new Integer[] { }), List.of()),
+                new TestCase(makeTree(new Integer[] { 1 }), List.of(1))
+                };
+         // @formatter:on
+        Solution s = new Solution();
+
+        for (TestCase test : tests) {
+            List<Integer> actual = s.postorderTraversal(test.in);
+            assert Objects.equals(test.expected, actual) : "postorderTraversal(%s) == %s, want %s".formatted(test.in,
+                    actual, test.expected);
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return List.of();
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        LinkedList<Integer> result = new LinkedList<>();
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.addFirst(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return result;
+    }
+
+    private static TreeNode makeTree(Integer[] values) {
+        if (values.length < 1) {
+            return null;
+        }
+        TreeNode root = new TreeNode(values[0]);
+        List<TreeNode> prevLevel = List.of(root);
+        int i = 1;
+        while (i < values.length) {
+            List<TreeNode> currentLevel = new ArrayList<>(prevLevel.size() * 2);
+            for (int j = 0; j < prevLevel.size() && i < values.length; j++) {
+                if (values[i] != null) {
+                    TreeNode leftNode = new TreeNode(values[i]);
+                    prevLevel.get(j).left = leftNode;
+                    currentLevel.add(leftNode);
+                }
+                i++;
+                if (i < values.length) {
+                    if (values[i] != null) {
+                        TreeNode rightNode = new TreeNode(values[i]);
+                        prevLevel.get(j).right = rightNode;
+                        currentLevel.add(rightNode);
+                    }
+                    i++;
+                }
+            }
+            prevLevel = currentLevel;
+        }
+        return root;
+    }
+
+}
--- README.md
+++ README.md
@@ -50,6 +50,7 @@ Solutions to problems from leetcode.com.
 [136](136). Single Number  
 [141](141). Linked List Cycle  
 [144](144). Binary Tree Preorder Traversal  
+[145](145). Binary Tree Postorder Traversal  
 [151](151). Reverse Words in a String  
 [162](162). Find Peak Element  
 [169](169). Majority Element  
